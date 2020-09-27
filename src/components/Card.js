bbb21f4450da5b81130a1e967076b54630e63a41
Funny Rose <fannyperretlombard@gmail.com>
2020-09-27 02:21:59 +0200

    Change css's footer; adding social media's links Linkedin and Github :octocat:

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,9 +6,10 @@ function Card(props) {
 
     return(
         <div className="d-inline-block css-card" onClick={(e) => props.click(props.item)}>
-            
+
             <img className="css-card-img" src={props.item.imgSrc} alt={props.item.imgSrc} />
             { props.item.selected && <CardInfo title={props.item.title} subTitle={props.item.subTitle} /> }
+            
         </div>
     )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,8 +13,8 @@ function Footer() {
     return(
         <footer className="mt-5">
             <Container fluid={true}>
-                <Row className="border-top justify-content-between p-3">
-                    <Col className="p-0 justify-content-end" md={6}>
+                <Row className="border-top p-3">
+                    <Col className="p-0" md={6} sm={12} xs={12}>
                         This site was made by Fanny Perret with  
                         {' '}
                         <img 
@@ -23,8 +23,27 @@ function Footer() {
                         width="15"
                         />, <Bootstrap size={15} /> and <Heart size={15} />.
                     </Col>
-                    <Col className="p-0 justify-content-end" md={6}>
+                    <Col className="p-0" md={3} sm={6} xs={4}>
                         © <Year />
+                    </Col>
+                    <Col className="p-0" md={3} sm={6} xs={8}>
+                        <a href="https://github.com/fannyperret">
+                            <img 
+                            alt="Black icon React"
+                            src={require('../assets/img/icons/github.png')} 
+                            width="30"
+                            />
+                            <span className="social-media">Github</span>
+                        </a>
+                        {' '}
+                        <a href="https://www.linkedin.com/in/perretfanny/">
+                            <img 
+                            alt="Black icon React"
+                            src={require('../assets/img/icons/linkedin.png')} 
+                            width="25"
+                            />
+                            <span className="social-media">Linkedin</span>
+                        </a>
                     </Col>
                 </Row>
             </Container>
